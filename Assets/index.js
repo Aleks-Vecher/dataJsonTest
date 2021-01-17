7b4fd17495f106aaa87a28a3d259bd9590a1c795
Aleks-Vecher <aleks.vecher@gmail.com>
2021-01-17 22:44:47 +0300

    third commit

--- Assets/index.js
+++ Assets/index.js
@@ -36,31 +36,62 @@ const getData = () => {
 }
 
 getData().then(data => {
-  const creatUrlArr = data.categories[0].videos.reduce((acc, item) => {
+  creatFilmListTable(data)(creatUrlArr)
+})
+
+const creatUrlArr = (data) => {
+  return data.categories[0].videos.reduce((acc, item) => {
     const sourceArr = item.sources[0].split('/')
     sourceArr.pop()
     return [...acc, {pic: sourceArr.concat(`${item.thumb}`).join('/'), ...item}]
   }, [])
+}
 
-  const createTable = (mass) => {
-    let table = document.getElementById('root');
-    table.innerHTML = '';
+const creatFilmListTable = (data) => (fn) => {
+  let videosArr = data.categories[0].videos
+  let tableFilm = document.querySelector('.filmContainerRow')
 
-    for (let el of mass) {
-      table.innerHTML += `<div class="videoContainer">
-      <div>
+  if (videosArr.length === 0) {
+    tableFilm.innerHTML = `<h2>There are no any films available</h2>`
+  } else {
+    tableFilm.innerHTML = '';
+
+    fn(data).forEach((el, i) => {
+      tableFilm.innerHTML += `<div class="videoContainer">
         <h3>${el.title}</h3>
-        <img onclick="window.open('${el.sources[0]}')" alt='pic' src=${el.pic}>
+        <h4 class="subtitle">${el.subtitle}</h4>
+      <div>
+        <img data-img="video" alt='pic' class='imgVideo' data-playVideoUrl=${el.sources[0]} src=${el.pic}>
       </div>
-      <p class="description">Description: ${el.description}</p>
+      <button class="showDescription" data-id="desc-${i}">show description</button>
+        <p class="description description_close" id="desc-${i}">${el.description}</p>
       </div>
-    `;
-    }
+      `;
+    })
   }
+}
 
-  createTable(creatUrlArr)
-})
+document.addEventListener('click', (e) => {
+      if (e.target.classList.contains('showDescription')) {
+        const showDescription = document.querySelector(`[data-id=${e.target.dataset.id}]`)
+        const description = document.getElementById(`${e.target.dataset.id}`)
 
+        if (description.classList.contains('description_close')) {
+          description.classList.remove('description_close')
+          showDescription.innerHTML = 'hide description'
+        } else {
+          description.classList.add('description_close')
+          showDescription.innerHTML = 'show description'
+        }
+      }
 
-
-
+      const {img, playvideourl} = e.target.dataset;
+      if (img === 'video') {
+        const player = document.querySelector('.videoSrc')
+        const source = document.getElementById('myVideo')
+        player.setAttribute('src', `${playvideourl}`)
+        source.load()
+        source.play()
+      }
+    }
+)
